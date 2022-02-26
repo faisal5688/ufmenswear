ef6d473d97a03d0b7fd444e1e89119c0df9c3995
Faisal <shahfaisal.patel@kpmg.aptaracorp.com>
2022-02-26 21:14:32 +0530

    update

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+/* All images belong to its Intellectual Property Owner @ http://burciagraphy.com/ */
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+UPDATE: This is absolutely horrible, I have a new respect for the workings of
+        javascript!! Althought, it is one of my favorite languages..
+
+        I suggest you view in a full window becuase i have not yet made images
+        responsive enough, so the images just look fat and stretchy.
+@ 3/19/13
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
+UPDATE: Used an image compressor @http://www.smushit.com/ysmush.it/ as well used
+        an html formatter @http://www.freeformatter.com/html-formatter.html.
+        Since load time was INSANE, i improved by getting rid of my CSS only image
+        pre-loader, compressing images, and uplaoded all images to google's image
+        hosting, SAME domain.
+        Made an image sprite for the buttons and logo.
+@ 3/20/2013
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+// Props to @scarlson1109: one of his test-cases assisted my solution so far
