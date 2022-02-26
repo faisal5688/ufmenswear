17bae8b66b1384142012c3ed9f1d84d1d2c95280
Faisal <shahfaisal.patel@kpmg.aptaracorp.com>
2022-02-26 23:12:43 +0530

    update background

--- creative/js/creative.js
+++ creative/js/creative.js
@@ -40,9 +40,72 @@
         offset: {
             top: 100
         }
-    })
+    });
+
+
+    var prealoadImage = function(callback){
+        var images =[]
+        $("#preload img").each(function(){
+            //alert($(this).attr('src'));
+            images.push($(this).attr('src'))
+        });
+        preloadPictures(images, function(){
+            if(callback){
+                callback()
+            }
+        });
+    }
+
+    var preloadPictures = function(pictureUrls, callback) {
+        var i,
+            j,
+            loaded = 0;
+    
+        for (i = 0, j = pictureUrls.length; i < j; i++) {
+            (function (img, src) {
+                img.onload = function () {                               
+                    if (++loaded == pictureUrls.length && callback) {
+                        callback();
+                    }
+                };
+    
+                // Use the following callback methods to debug
+                // in case of an unexpected behavior.
+                img.onerror = function () {};
+                img.onabort = function () {};
+    
+                img.src = src;
+            } (new Image(), pictureUrls[i]));
+        }
+    };
+    
+
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+      
+    var rndInt = randomIntFromInterval(1, 9);
+    var updateBackImage = function(imgNum){
+        var imageUrl = "./src/img/slider/shop_image_"+imgNum+".jpg";
+        //alert(imageUrl)
+        $("header").css("background-image", "url(" + imageUrl + ")");
+    }
+    prealoadImage(function(){
+        
+    });
+    updateBackImage(rndInt);
+    setInterval(function(){
+        rndInt = randomIntFromInterval(1, 9);
+        updateBackImage(rndInt)
+    }, 3000);
+
+    var updateImageRandomly = function(){
+        
+    }
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+
